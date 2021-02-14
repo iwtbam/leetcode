92f228fb0970f1f0237d9151cdd44615b69505d5
iwtbam <1161784945@qq.com>
2021-02-14 22:14:56 +0800

    fix : 删除冗余文件

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args){
-        // int val = Integer.parseInt("0050");
-        String s = String.format("%05d", 50);
-        System.out.println(s);
-        return;
-    }
-}
